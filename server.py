67c0599706c40803f679cf696bce0ce2e820764b
AXREK24 <kevinalexisramirezecheverri@gmail.com>
2024-09-28 23:46:12 +0000

    Mundo guardado 2024-09-28

--- server.py
+++ server.py
@@ -43,7 +43,7 @@ if not os.path.exists("./.gitignore"):
 def download_latest_release(download_path='.'):
 	mirror = "https://elyxdev.github.io/latest"
 	pet = requests.get(mirror)
-	if pet.status_code == 200:
+	if pet.status_code == 200:1
 		data = pet.json()
 		url = data.get('latest')
 		version = url.split("/")[-1]
